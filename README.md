eb0bd87de318e16d7d6dd5db962ad79eb118167e
paynejc <paynejc84@gmail.com>
2022-02-16 01:12:11 -0500

    Added Password variables

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ User will be present with a series of prompts for password criteria
 Once criteria is selected, a password will be generated based upon that. 
 
 https://paynejc.github.io/PasswordGenerator/
+![Password Generator Screenshot]()
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
-// Assignment Code
+// upper/lower characters, numbers, symbols
+
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+// Prompt for password options
+//"Do you want questions...."
+//Validate input
+//password length >8 < 128
+//Pick at least one option
+//Get the required characters
+
+
+//Put the password on the screen
+
+//Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +26,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
